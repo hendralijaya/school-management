54c6156a1da9de72b92938f4bcd0b4601929f66d
hendralijaya <hendralijaya@gmail.com>
2023-07-21 16:01:21 +0700

    documentation for ruang api done

--- app/OpenApi/Schemas/API/v1/Ruang/UpdateRuangSchema.php
+++ app/OpenApi/Schemas/API/v1/Ruang/UpdateRuangSchema.php
@@ -2,15 +2,16 @@
 
 namespace App\OpenApi\Schemas\API\v1\Ruang;
 
-use GoldSpecDigital\ObjectOrientedOAS\Contracts\SchemaContract;
+use Vyuldashev\LaravelOpenApi\Contracts\Reusable;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Not;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\AllOf;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\AnyOf;
-use GoldSpecDigital\ObjectOrientedOAS\Objects\Not;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\OneOf;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
 use Vyuldashev\LaravelOpenApi\Factories\SchemaFactory;
+use GoldSpecDigital\ObjectOrientedOAS\Contracts\SchemaContract;
 
-class UpdateRuangSchema extends SchemaFactory
+class UpdateRuangSchema extends SchemaFactory implements Reusable
 {
     /**
      * @return AllOf|OneOf|AnyOf|Not|Schema
